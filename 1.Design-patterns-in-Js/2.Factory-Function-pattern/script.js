cfc649e59ebd31acf5b83046d171e35f2959d847
Avanish Chaurasiya <avanishchaurasiya71@gmail.com>
2025-12-13 14:11:54 +0530

    write a simple factory fn program

---
+++ 1.Design-patterns-in-Js/2.Factory-Function-pattern/script.js
@@ -0,0 +1,36 @@
+// Simple Factory Fucntion
+
+function createVehicle( name, price, stock) {
+    stock = stock;
+
+    return {
+        name,
+        price,
+
+        checkStock() {
+            console.log(`there are ${stock} pieces in the stock`);
+
+        },
+
+        setStock(val) {
+            stock += val;
+            console.log(`now thw new stock is: ${stock} pieces`);
+
+        },
+
+        buy(val) {
+            if (val <= stock) {
+                stock -= val;
+                console.log(`you ordered ${val} pieces`);
+
+            } else {
+                console.error(`not enough stock !!!  Toatl pieces in stock: ${stock}`);
+            }
+        }
+    }
+
+}
+
+
+let platina = createVehicle("Bajaj Platina", 96000, 15);
+platina.checkStock()
