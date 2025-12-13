7bc333182a58113f8c084e6b450734f9615b44c0
Avanish Chaurasiya <avanishchaurasiya71@gmail.com>
2025-12-13 14:26:40 +0530

    write a better program using Factory fn pattern

--- 1.Design-patterns-in-Js/2.Factory-Function-pattern/script.js
+++ 1.Design-patterns-in-Js/2.Factory-Function-pattern/script.js
@@ -1,6 +1,6 @@
 // Simple Factory Fucntion
 
-function createBike( name, price, stock) {
+function createBike(name, price, stock) {
     stock = stock;
 
     return {
@@ -33,10 +33,87 @@ function createBike( name, price, stock) {
 
 
 let platina = createBike("Bajaj Platina", 96000, 15);
-platina.checkStock()
+// platina.checkStock()
 
 
 
 // now a good a better Factory Function program
 
 
+
+function createVehicle(type, name, price, stock) {
+    stock = stock;
+    if (type === "bike") {
+
+        return {
+
+            name,
+            price,
+            vehicleType() {
+                console.log("bike");
+
+            },
+
+            checkStock() {
+                console.log(`there are ${stock} ${name}s in the stock`);
+
+            },
+
+            setStock(val) {
+                stock += val;
+                console.log(`now thw new stock is: ${stock}s`);
+
+            },
+
+            buy(val) {
+                if (val <= stock) {
+                    stock -= val;
+                    console.log(`you ordered ${val} ${name}s - now there are Toatl ${stock} ${name}s in the stock `);
+
+                } else {
+                    console.error(`not enough stock !!!  Toatl ${name}s in stock: ${stock}`);
+                }
+            }
+        }
+    }
+
+    if (type === "car") {
+        return {
+            name,
+            price,
+
+            vehicleType() {
+                console.log("Car");
+
+            },
+
+            checkStock() {
+                console.log(`there are ${stock} ${name}s in the stock`);
+
+            },
+
+            setStock(val) {
+                stock += val;
+                console.log(`now thw new stock is: ${stock} ${name}s`);
+
+            },
+
+            buy(val) {
+                if (val <= stock) {
+                    stock -= val;
+                    console.log(`you ordered ${val} ${name}s - now there are Toatl ${stock} ${name}s in the stock `);
+
+                } else {
+                    console.error(`not enough stock !!!  Toatl ${name}s in stock: ${stock}`);
+                }
+            }
+        }
+    }
+}
+
+
+let gWagonR = createVehicle("car", "G WagonR", 500000, 12);
+console.log(gWagonR.checkStock());
+
+
+let Pulsur = createVehicle("bike", "Bajaj Pulsur", 175000, 21);
