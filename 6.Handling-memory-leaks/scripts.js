c84cc97c13b63ad1013fce3c0beaa7eb0826cf89
Avanish Chaurasiya <avanishchaurasiya71@gmail.com>
2025-12-16 15:34:26 +0530

    Handling Memory leaks

--- 6.Handling-memory-leaks/scripts.js
+++ 6.Handling-memory-leaks/scripts.js
@@ -1,13 +1,13 @@
 let count = 0;
 
-const interval1 = setInterval(()=>{
-   if (count < 10) {
-       count += 1;
-       console.log(count);
-   }   // interval code tab bhi chalta rahega jab condition puri ho chuki ho agar else block nhi hai to
-    else{
+const interval1 = setInterval(() => {
+    if (count < 10) {
+        count += 1;
+        console.log(count);
+    }   // interval code tab bhi chalta rahega jab condition puri ho chuki ho agar else block nhi hai to
+    else {
         clearInterval(interval1);
         console.log("Interval Cleared ");
-        
+
     }
 }, 500)
