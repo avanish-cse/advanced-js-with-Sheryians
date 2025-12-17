8d98717eb0d609673f6df000358a761f2c713bbb
Avanish Chaurasiya <avanishchaurasiya71@gmail.com>
2025-12-17 23:07:23 +0530

    creating custom utilities

--- 8.Custom-utilities/script.js
+++ 8.Custom-utilities/script.js
@@ -1,6 +1,21 @@
 //  Js inbuilt map function
 
-let arr = [1,2, 3, 4, 5];
-let newArr = arr.map((enrty) => enrty*2 );
-console.log(newArr);
+let arr = [1, 2, 3, 4, 5];
+let arr2 = arr.map((enrty) => enrty * 2);
+console.log(arr2);
+
+//  Let's create our custom map function
+
+const doKaTable = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+
+function myMapFn(array, callback) {
+    let newArr = [];
+
+    for (let i = 0; i < array.length; i++) {
+
+    }
+    ;
+
+}
+
 
